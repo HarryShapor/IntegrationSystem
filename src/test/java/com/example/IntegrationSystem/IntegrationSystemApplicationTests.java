60443394a2fe9925f92e2691ea32cde238a36e43
harryshapor <shaporenkoandrew@gmail.com>
2025-07-11 09:53:01 +0300

    update files

---
+++ src/test/java/com/example/IntegrationSystem/IntegrationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.IntegrationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
